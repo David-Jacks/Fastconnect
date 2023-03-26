74e0223c0c5f41408132e84220dfa6d112e221f2
Faith Champ <djackfaith0.5@gmail.com>
2023-03-26 17:55:42 +0000

    new

--- mydata.js
+++ mydata.js
@@ -1,17 +1,15 @@
-
 const mysql = require("mysql2");
-
+// mysql://b407aa85827dc5:1adbd647@us-cdbr-east-06.cleardb.net/heroku_e730e4e1eab6206?reconnect=true
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "logout.commysql",
-    database: "fastconnect",
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b407aa85827dc5",
+  password: "1adbd647",
+  database: "heroku_e730e4e1eab6206",
 });
 
-connection.connect(() =>{
-    console.log("database connected");
+connection.connect(() => {
+  console.log("database connected");
 });
 
-
-  module.exports = connection;
+module.exports = connection;
